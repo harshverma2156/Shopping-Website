8d625760279ee70b78852bb3599e0de20d2a0aa4
Harsh Verma <harshvermadev@gmail.com>
2026-05-21 19:15:16 +0530

    Added Product card and images: with image link

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import ProductCard from "./components/ProductCard"
 import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div>
       <Navbar />
-      <h1>Home Page</h1>
+      <h1>Our Product</h1>
+
+      <ProductCard
+      name="Nike Shoes"
+      price="2,999"
+      image="https://images.unsplash.com/photo-1542291026-7eec264c27ff"
+      />
+
+       <ProductCard
+        name="Smart Watch"
+        price="4999"
+        image="https://images.unsplash.com/photo-1523275335684-37898b6baf30"
+      />
+
+      <ProductCard
+        name="Headphones"
+        price="1999"
+        image="https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
+      />
     </div>
   )
 }
